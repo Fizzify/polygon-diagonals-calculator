1106da13a99c58469f1f1ac50c1d6e55196e057a
Fizzify <21076@brightwoods.edu.ph>
2022-03-24 07:31:38 +0800

    Made more functionality.

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ const result = document.querySelector("#result");
 function diagonalsPolygon() {
   var input = prompt("How many sides does the polygon have?");
   var diagonals = (input * (input - 3)) / 2;
-  result.textContent =
+
+  if (input <= 3) {
+    result.textContent =
+    "This polygon has no diagonals. ";
+  } else {
+    result.textContent =
     "This " + input + "-gon" + " has " + diagonals + " diagonals.";
+  }
 }
 
 diagonalsPolygon();
